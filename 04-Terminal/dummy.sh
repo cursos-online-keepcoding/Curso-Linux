338e6645d457fc7edb44a83ef57e5cca64ad07e3
Julián García-Sotoca Pascual <juliansotoca@gmail.com>
2020-04-18 19:58:12 +0200

    add disk and port forwarding to vagrantfile

---
+++ 04-Terminal/dummy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# keepcoding
+# Autor: Julian Garcia-Sotoca <juliansotoca@gmail.com>
+# Abr. 2020
+#
+# Script que únicamente se queda ejecutnado in eternum
+
+while true; do
+	sleep 5
+	echo "I'm here"
+done
+
+exit 0
